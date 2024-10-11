f145a54f9bad2118ab90b3a83c850c5e23ea3b0b
Felipe Camara <78320789+fcamaraneto@users.noreply.github.com>
2024-10-11 10:17:09 +0200

    Update 3_Short-Circuit.py

--- pages/3_Short-Circuit.py
+++ pages/3_Short-Circuit.py
@@ -7,7 +7,7 @@ import numpy as np
 #import scipy.special as spios
 #import plotly.express as px
 
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 
 #  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 #                                     SETTINGS
